bc20b61c1ae6cfd37fed13dbacccfbb96c2c921d
Michael Lundholm <m.lundholm3@gmail.com>
2020-12-31 13:44:10 -0800

    initial commit

---
+++ ui/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  gateway: '',
+  callback: 'http://localhost:4200/callback',
+  domain: 'lundholm.us.auth0.com',
+  clientId: 'kTYsnuDDIeWneBciQ97sXyQzU6i4RW7P',
+  audience: 'https://my-golang-api/'
+};
